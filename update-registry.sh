e5e107db60bc660fffc2d4921e7f879a3bd4a17f
xtao <xutao881001@gmail.com>
2016-05-30 15:01:21 +0800

    Bump 0.1.16

--- update-registry.sh
+++ update-registry.sh
@@ -1,3 +1,3 @@
 IMAGE_ID=$1
-docker tag $IMAGE_ID registry.aliyuncs.com/laincloud/networkd-binary:0.1.15
-docker push registry.aliyuncs.com/laincloud/networkd-binary:0.1.15
+docker tag $IMAGE_ID registry.aliyuncs.com/laincloud/networkd-binary:0.1.16
+docker push registry.aliyuncs.com/laincloud/networkd-binary:0.1.16
